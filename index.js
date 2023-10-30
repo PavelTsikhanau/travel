e2db6c4f862987514da3400368e2cb49ecd747d4
Pavel Tsikhanau <tishka1991@gmail.com>
2023-10-30 11:20:01 +0300

    feat: add endless slider

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let offset = 0;
+const wrapper = document.querySelector('.wrapper');
 
+document.querySelector('.destinations-button').addEventListener('click', function() {
+  offset += 860;
+  if (offset > 1600) {
+    offset = 0;
+  }
+  wrapper.style.left = -offset + 'px';
+
+})
