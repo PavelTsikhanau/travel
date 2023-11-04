56fe5a750e67965b4c563ecab0cb3fd7c0cb97b0
Pavel Tsikhanau <tishka1991@gmail.com>
2023-11-04 23:20:30 +0300

    feat: finish mob layout & add dskt slide function

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 let offset = 0;
-const wrapper = document.querySelector('.wrapper');
+const wrapper = document.querySelector('.destinations-wrapper');
+let index = 0;
+
 
 document.querySelector('.destinations-button').addEventListener('click', function() {
   offset += 860;
-  if (offset > 1600) {
+  if (offset > 1720) {
     offset = 0;
   }
   wrapper.style.left = -offset + 'px';
+  // const slides = wrapper.children;
+  // console.log(wrapper.children);
 
-})
+  // const firstSlide = wrapper.removeChild(slides[0]);
+  // wrapper.appendChild(slides[index]);
+  // index += 1;
+
+  // console.log(wrapper.children);
+});
+
+
+  const loginBtn = document.querySelector('.header-button');
+  loginBtn.addEventListener('click', function () {
+    // document.querySelector('.popup').style.display = 'flex';
+    document.querySelector('.popup').classList.remove('hidden');
+  })
+
