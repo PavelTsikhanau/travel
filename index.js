818ea6cc0cfb15b350460230df61ef5c0af7cca1
Pavel Tsikhanau <tishka1991@gmail.com>
2023-11-06 11:21:31 +0300

    feat: add desktop slider's functionality

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-let offset = 0;
-const wrapper = document.querySelector('.destinations-wrapper');
-let index = 0;
+let offset = 0,
+    index = 1;
 
+const wrapper = document.querySelector('.destinations-wrapper'),
+      indicator = document.querySelectorAll('.destinations-indicator'),
+      slide = document.querySelectorAll('.destinations-slide');
 
-document.querySelector('.destinations-button').addEventListener('click', function() {
+//окрашиваем индикатор 
+indicator[index].style.background = '#F2785C';
+
+function turnTheSlideRight () {
   offset += 860;
-  if (offset > 1720) {
-    offset = 0;
-  }
+  indicator[index].style.background = 'rgba(242, 120, 92, 0.5)';
+  indicator[index + 1].style.background = '#F2785C';
+  index += 1;
+  if (offset >= 860) {
+    document.querySelector('.destination-click-right').removeEventListener('click', turnTheSlideRight);
+    document.querySelector('.destination-click-left').addEventListener('click', turnTheSlideLeft);
+  } 
   wrapper.style.left = -offset + 'px';
-  // const slides = wrapper.children;
-  // console.log(wrapper.children);
+}
 
-  // const firstSlide = wrapper.removeChild(slides[0]);
-  // wrapper.appendChild(slides[index]);
-  // index += 1;
+function turnTheSlideLeft () {
+  offset -= 860;
+  indicator[index].style.background = 'rgba(242, 120, 92, 0.5)';
+  indicator[index - 1].style.background = '#F2785C';
+  index -= 1;
+  if (offset <= -860) {
+    document.querySelector('.destination-click-left').removeEventListener('click', turnTheSlideLeft);
+    document.querySelector('.destination-click-right').addEventListener('click', turnTheSlideRight);
+  } 
+  wrapper.style.left = -offset + 'px';
+}
 
-  // console.log(wrapper.children);
+document.querySelector('.destination-click-right').addEventListener('click', turnTheSlideRight);
+document.querySelector('.destination-click-left').addEventListener('click', turnTheSlideLeft);
+
+console.log(indicator[index], slide[index+1]);
+
+
+const loginBtn = document.querySelector('.header-button');
+loginBtn.addEventListener('click', function () {
+  document.querySelector('.popup').classList.remove('hidden');
 });
 
-
-  const loginBtn = document.querySelector('.header-button');
-  loginBtn.addEventListener('click', function () {
-    // document.querySelector('.popup').style.display = 'flex';
-    document.querySelector('.popup').classList.remove('hidden');
-  })
+document.querySelector('.popup').addEventListener('click', function () {
+  document.querySelector('.popup').classList.add('hidden');
+});
 
